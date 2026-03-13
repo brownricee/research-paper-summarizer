ab2324313b4d8251c06c70e40f54d328b98ffbbc
brownricee <ryaankhawaja09@gmail.com>
2026-03-13 14:38:25 -0500

    updated main.py so main actually runs now

--- main.py
+++ main.py
@@ -2,13 +2,18 @@ import torch
 import numpy as np
 import matplotlib as plt
 
+from src.preprocessing import clean_text
 from src.extraction import getTextFromPDF
 from src.chunking import chunk_text
 
 def main():
-    pdf_path = "C:\Users\ryaan\OneDrive\Desktop\paper-summarizer\example_research_paper.pdf"
+    pdf_path = "C:\\Users\\ryaan\\OneDrive\\Desktop\\paper-summarizer\\example_research_paper.pdf"
 
     cleaned_text = getTextFromPDF(pdf_path)
 
     chunks = chunk_text(cleaned_text)
 
+
+
+if __name__ == "__main__":
+    main()
