a4e75d1be13c312d39607ac8fe50f8f922d13576
brownricee <ryaankhawaja09@gmail.com>
2026-03-28 13:24:16 -0500

    created basic setup of ranking.py

--- src/ranking.py
+++ src/ranking.py
@@ -1,1 +1,7 @@
+from sentence_transformers import SentenceTransformer
+from sklearn.metrics.pairwise import cosine_similarity
 
+model = SentenceTransformer('all-MiniLM-L6-v2')
+
+def rank_chunks(chunks):
+    pass
