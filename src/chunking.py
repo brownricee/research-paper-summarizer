1532be89caa9d8d36c30e74ae9de0a175089f3a6
brownricee <ryaankhawaja09@gmail.com>
2026-04-05 17:02:06 -0500

    updated chunking.py to accept dictionary instead of raw text

--- src/chunking.py
+++ src/chunking.py
@@ -4,7 +4,7 @@ import subprocess
 import sys
 
 
-def chunk_text(text, sentences_per_chunk=5):
+def chunk_text(sections, sentences_per_chunk=5):
   # Ensure en_core_web_sm is installed
   try:
     nlp = spacy.load("en_core_web_sm")
@@ -13,13 +13,20 @@ def chunk_text(text, sentences_per_chunk=5):
     subprocess.check_call([sys.executable, "-m", "spacy", "download", "en_core_web_sm"])
     nlp = spacy.load("en_core_web_sm")
 
-  doc = nlp(text)
-  sentences = [sent.text for sent in doc.sents]
-  chunks = []
 
-  for i in range(0, len(sentences), sentences_per_chunk):
+  all_chunks = []
+
+  for section, content in sections.items():
+     doc = nlp(text=content)
+     sentences = [sent.text for sent in doc.sents]
+
+     chunks = []
+
+     for i in range(0, len(sentences), sentences_per_chunk):
       # slices the sentences so we have 5 per chunk
       chunk = " ".join(sentences[i:i + sentences_per_chunk])
-      chunks.append(chunk)
+      chunks.append({"section": section, "text": chunk})
 
-  return chunks
+     all_chunks.extend(chunks)
+
+  return all_chunks
