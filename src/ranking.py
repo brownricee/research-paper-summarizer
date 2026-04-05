21a1d27494800100d533b0946b70b677e945d106
brownricee <ryaankhawaja09@gmail.com>
2026-04-05 18:00:34 -0500

    changed comments

--- src/ranking.py
+++ src/ranking.py
@@ -8,11 +8,11 @@ def rank_chunks(chunks):
     texts = [chunk["text"] for chunk in chunks]
     embeddings = model.encode(texts)
 
-    # Compute centroid (average meaning of paper)
+    # Compute centroid (which chunks best represent overall meaning of paper)
     centroid = np.mean(embeddings, axis=0)
     # score each chunk by cosine similarity to centroid
     scores = cosine_similarity(embeddings, centroid.reshape(1, -1))
-    # sort the chunks in ascending order and flatten so its a 1-dimensional array
+    # sort in ascending order and flatten so its a 1-dimensional array
     sorted_indices = np.argsort(scores.flatten(), axis=None)[::-1]
 
     # copies previous dictionary and continues adding to it (now with scores)
