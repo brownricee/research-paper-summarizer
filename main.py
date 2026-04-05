96a1b2d3891537aec64f1c6aeada951cc1a2e621
brownricee <ryaankhawaja09@gmail.com>
2026-04-05 17:28:31 -0500

    added ranking to main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import matplotlib as plt
 from src.preprocessing import clean_text
 from src.extraction import getTextFromPDF
 from src.chunking import chunk_text
+from src.ranking import rank_chunks
 
 def main():
     pdf_path = "C:\\Users\\ryaan\\OneDrive\\Desktop\\paper-summarizer\\example_research_paper.pdf"
@@ -12,6 +13,8 @@ def main():
     sections = getTextFromPDF(pdf_path)
 
     chunks = chunk_text(sections)
+
+    ranked_chunks = rank_chunks(chunks)
 
 
 
