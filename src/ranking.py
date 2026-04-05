9bbe53eb51d1fc6ac19d51c12800ec1baebb9e27
brownricee <ryaankhawaja09@gmail.com>
2026-04-05 17:22:05 -0500

    changed ranking.py to create embeddings over dictionary correclty instead of raw text

--- src/ranking.py
+++ src/ranking.py
@@ -4,8 +4,10 @@ from sklearn.metrics.pairwise import cosine_similarity
 model = SentenceTransformer('all-MiniLM-L6-v2')
 
 def rank_chunks(chunks):
-    chunk_embeddings = model.encode(chunks)
-    
+    texts = [chunk["text"] for chunk in chunks]
+    embeddings = model.encode(texts)
+
+
     # compute centroid
     # score each chunk by cosine similarity to centroid
     # return chunks sorted by score!
