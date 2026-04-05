6d50b1bc621691d407a27069038129e6f5d76d83
brownricee <ryaankhawaja09@gmail.com>
2026-04-05 16:09:58 -0500

    encode chunks

--- src/ranking.py
+++ src/ranking.py
@@ -4,4 +4,9 @@ from sklearn.metrics.pairwise import cosine_similarity
 model = SentenceTransformer('all-MiniLM-L6-v2')
 
 def rank_chunks(chunks):
+    chunk_embeddings = model.encode(chunks)
+    
+    # compute centroid
+    # score each chunk by cosine similarity to centroid
+    # return chunks sorted by score!
     pass
