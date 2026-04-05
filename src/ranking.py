08f3831bd4039d146d6e36df32d4408211f552df
brownricee <ryaankhawaja09@gmail.com>
2026-04-05 17:46:45 -0500

    added functionality to rank chunks and use cosine similarity to show how things are related

--- src/ranking.py
+++ src/ranking.py
@@ -1,5 +1,6 @@
 from sentence_transformers import SentenceTransformer
 from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
 
 model = SentenceTransformer('all-MiniLM-L6-v2')
 
@@ -7,8 +8,12 @@ def rank_chunks(chunks):
     texts = [chunk["text"] for chunk in chunks]
     embeddings = model.encode(texts)
 
-
-    # compute centroid
+    # Compute centroid (average meaning of paper)
+    centroid = np.mean(embeddings, axis=0)
     # score each chunk by cosine similarity to centroid
-    # return chunks sorted by score!
-    pass
+    scores = cosine_similarity(embeddings, centroid.reshape(1, -1))
+    # sort the chunks in ascending order and flatten so its a 1-dimensional array
+    sorted_indices = np.argsort(scores.flatten(), axis=None)[::-1]
+
+    # copies previous dictionary and continues adding to it (now with scores)
+    return [{**chunks[i], "score": scores.flatten()[i]} for i in sorted_indices]
