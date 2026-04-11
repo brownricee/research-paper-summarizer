29e027c2f82c6cc8ae3aea3dc141fc334de6bc20
brownricee <ryaankhawaja09@gmail.com>
2026-04-10 21:26:47 -0500

    implement summary aggregation logic in aggregation.py

--- src/aggregation.py
+++ src/aggregation.py
@@ -1,4 +1,9 @@
 from src.summarization import summarizer
 
 def aggregate_summary(chunk_summaries):
-    pass
+    if chunk_summaries:
+        chunk_summaries = summarizer(chunk_summaries, max_length=300, min_length=100, do_sample=False)
+        summary_text = chunk_summaries[0]["summary_text"]
+        return summary_text
+    else:
+        return ""
